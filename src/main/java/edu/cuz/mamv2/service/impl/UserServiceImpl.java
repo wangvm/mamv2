de5e7b3e6d320c94142d22449bd4e254fa92155e
VM <465132131@qq.com>
2022-04-08 22:54:47 +0800

    🐛 fix 修复查询用户返回数据不完整
    Signed-off-by: VM <465132131@qq.com>

--- src/main/java/edu/cuz/mamv2/service/impl/UserServiceImpl.java
+++ src/main/java/edu/cuz/mamv2/service/impl/UserServiceImpl.java
@@ -50,7 +50,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
     @Override
     public Page<User> queryUserList(Integer status, String order, Integer isAsc, Integer current, Integer pageSize) {
         QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-        queryWrapper.select("account", "username", "createTime")
+        queryWrapper.select("account", "createTime", "password", "role", "username")
                 .eq("deleted", status)
                 .orderBy(true, isAsc > 0 ? true : false, order);
         Page<User> page = userMapper.selectPage(new Page<User>(current, pageSize, true),
